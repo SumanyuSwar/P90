b8936f94a572f082cc9553fffb23a1b420461d16
Sudip Swar <155371916+SumanyuSwar@users.noreply.github.com>
2024-06-17 22:13:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 var player1_score = 0
 var player2_score = 0
 var answer = 0
-var user1
-var user2
-localStorage.removeItem("User 1")
-localStorage.removeItem("User 2")
+var user1 = ""
+var user2 = ""
 function addUser(){
     localStorage.setItem("User 1", document.getElementById("user_in_1").value)
     localStorage.setItem("User 2", document.getElementById("user_in_2").value)
     location.href = "http://127.0.0.1:5500/New%20folder/index2.html"
-    user1 = localStorage.getItem("User 1")
-    user2 = localStorage.getItem("User 2")
+    user1 = String(localStorage.getItem("User 1"))
+    user2 = String(localStorage.getItem("User 2"))
 }
 function setup(){
     document.getElementById("player1_name").innerHTML = user1 + ": " + player1_score
